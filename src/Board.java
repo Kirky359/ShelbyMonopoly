d1a34b7c64c08c43e5852274118d7c24b0db2e83
Nikita Chupahin <nik123chupahin@gmail.com>
2023-10-24 09:33:26 +0300

    edit main file

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class Board {
+    public Board(ArrayList<Player> players, Dice dice, Jail jail){
+
+    }
+
 }
--- src/Monopoly.java
+++ src/Monopoly.java
@@ -1,7 +1,16 @@
-
+import java.util.ArrayList;
 public class Monopoly {
-    public static void main(String[] args) {
-        System.out.println("Dice");
-        System.out.println("Moon");
-        }
+    public static void main(String[] args){
+        int amountPlayers = 2;
+        Dice dice = new Dice();
+        Jail jail = new Jail();
+        ArrayList<Player> players = createPlayers(amountPlayers);
+        Board board = new Board(players, dice, jail);
+        Game game = new Game(players, board, dice, jail);
     }
+
+    static ArrayList<Player> createPlayers(int numPlayers){
+        ArrayList<Player> players = new ArrayList<>();
+        return players;
+    }
+}
