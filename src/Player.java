f605a059360272314f55fb29e49817ceb7688f99
kirkhammett <@kirky359>
2023-10-24 21:38:13 +0300

    move func addMoney etc

--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,7 @@ public class Player {
     private String name;
     private int money;
     private int position;
+    
     private ArrayList<Square> properties;
 
     public String getName() {
@@ -25,11 +26,26 @@ public class Player {
         properties = new ArrayList<Square>();
     }
 
+
     private boolean owns(){
         return false;
     }
 
     private void buy(){
-
     }
+    public void addMoney(int addMoney){
+        this.money += addMoney;
+    }
+    public void move(int numSquares){
+        position += numSquares;
+
+        //if pass GO
+        if(position >= 40){
+            System.out.println(name + " passed GO and collected $200");
+            money += 200;
+            position %= 40;
+        }
+    }
+
+
 }
