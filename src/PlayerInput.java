8d09b4d4d00973fd22603e1c6b285dd09ab6f263
kirkhammett <@kirky359>
2023-10-25 20:32:47 +0300

    selectOptions

--- src/PlayerInput.java
+++ src/PlayerInput.java
@@ -1,29 +1,15 @@
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class PlayerInput {
     public static String read() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
-    public static Object selectOptions(List<?> list, String message){
-
-        if(list.size() == 0) {
-            return null;
-        }
-
-        System.out.println(message + " (Enter a number 1 - " + list.size() + ")");
-
-        for(int i = 1; i <= list.size(); i++){
-            System.out.println(i + ". " + list.get(i - 1).toString());
-        }
-
-        try {
-            int input = Integer.parseInt(read());
-            return list.get(input - 1);
-        } catch(NumberFormatException | IndexOutOfBoundsException e){
-            System.out.println("Enter a valid number");
-            return selectOptions(list, message);
-        }
+    public static Object selectOptions(){
+        System.out.println("Enter 1 to buy property\n Enter 2 to end your turn ");
+        return Integer.parseInt(read());
     }
 }
