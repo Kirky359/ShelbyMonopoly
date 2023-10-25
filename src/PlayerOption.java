a2a5d3ab90aa507a8857792747e01a44c9cd0f4b
ArthurMonopolyMaster <arthurmolchanov54@gmail.com>
2023-10-25 11:40:46 +0300

    add PlayerOption

---
+++ src/PlayerOption.java
@@ -0,0 +1,13 @@
+public class PlayerOption {
+    String message;
+
+    public PlayerOption(String message){
+        this.message = message;
+    }
+
+    abstract public void action();
+
+    public String toString(){
+        return message;
+    }
+}
