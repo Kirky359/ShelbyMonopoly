72dc055e591ead81e63ee64f996cd80591ac5c6d
Nikita Chupahin <nik123chupahin@gmail.com>
2023-10-25 20:40:13 +0300

    Update Game class

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Game {
     private int sizeBoard = 40;
-    Player[] players;
-    Square[] board = new Square[sizeBoard];
 
-    public Game(int amountPlayers) {
-        players = new Player[amountPlayers];
-        new Cube().rollCube();
+    private Cube cube;
+    private Board board;
+    private ArrayList<Player> players = new ArrayList<Player>();
+
+    public Game( ArrayList<Player> players, Board board, Cube cube) {
+        this.cube = cube;
+        this.players = players;
+        this.board = board;
     }
 
     // Pass the turn to the next player in the game
-    private void turn(Player currentPlayer){
+    private void turnPlayer(Player currentPlayer){
         System.out.println("\n" + currentPlayer.getName() + "'s turn!\n");
+        System.out.println("\nCash :" + currentPlayer.getMoney());
+        System.out.println("\nPosition: " + currentPlayer.getPosition());
+
+        currentPlayer.move(cube.rollCube());
+
+        showPlayerOptions(currentPlayer);
+    }
+
+    private void showPlayerOptions(Player currentPlayer){
+
     }
 }
