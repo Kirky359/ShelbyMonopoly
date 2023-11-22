a9f63369bb63c97d832195a37bf1ad02a860e901
Nikita Chupahin <nik123chupahin@gmail.com>
2023-11-22 20:40:27 +0200

    PlayerOption class added

---
+++ src/Game/PlayerOption.java
@@ -0,0 +1,14 @@
+package Game;
+public abstract class PlayerOption {
+    String message;
+
+    public PlayerOption(String message){
+        this.message = message;
+    }
+
+    abstract public void action();
+
+    public String toString(){
+        return message;
+    }
+}
