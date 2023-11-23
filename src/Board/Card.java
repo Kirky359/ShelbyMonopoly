d8b113e68e5241900e806431434096b727bccd20
ArthurMonopolyMaster <arthurmolchanov54@gmail.com>
2023-11-23 11:56:28 +0200

    add HouseRepairCard class

--- src/Board/Card.java
+++ src/Board/Card.java
@@ -56,14 +56,34 @@ class HouseRepairCard extends CollectCard {
     private int perHouse;
     private int perHotel;
 
-    public HouseRepairCard(int perHouse, int perHotel, String text) {
+    public HouseRepairCard(int perHouse, int perHotel, String text){
         super(0, text);
         this.perHouse = perHouse;
         this.perHotel = perHotel;
     }
 
+    private int calculateFee(Player player){
+        int fee = 0;
 
+        for(PropertyColors p : player.getOwnColorGroupList()){
+            if(p.getNumHouses() == 5){
+                fee += perHotel;
+            } else if (p.getNumHouses() > 0) {
+                fee += perHouse * p.getNumHouses();
+            }
+        }
+
+        amount = fee;
+        return fee;
+    }
+
+    public void doAction(Player player){
+        player.addMoney(-calculateFee(player));
+    }
 }
+
+
+
 class MoveCard extends Card {
     private int numSquares;
     private Board board;
