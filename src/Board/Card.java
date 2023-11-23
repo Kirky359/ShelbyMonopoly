06f28c743791102db4944830317f1f9683087c53
SaulGoodman <mihailbojko144@gmail.com>
2023-11-23 12:17:19 +0200

    Board class modified (new functional added),  Card class modified(movetoCard added)

--- src/Board/Card.java
+++ src/Board/Card.java
@@ -82,7 +82,29 @@ class HouseRepairCard extends CollectCard {
     }
 }
 
+class MoveToCard extends Card {
+    private int[] index;
+    private Board board;
 
+    public MoveToCard(int[] index, Board board, String text){
+        super(text);
+        this.index = index;
+        this.board = board;
+    }
+
+    public void doAction(Player player){
+        int minimumDistance = 40;
+
+        for(int i = 0; i < index.length; i++){
+            int distanceToIndex = (40 + index[i] - player.getPosition()) % 40;
+            if ((40 + index[i] - player.getPosition()) < minimumDistance) {
+                minimumDistance = distanceToIndex;
+            }
+        }
+
+        player.move(minimumDistance, board);
+    }
+}
 
 class MoveCard extends Card {
     private int numSquares;
