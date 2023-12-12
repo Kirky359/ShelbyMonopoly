a66417c1c941f8c9f6ed05156538545c730066a2
SaulGoodman <mihailbojko144@gmail.com>
2023-12-12 18:17:35 +0200

    For class Square interface Cloneable implemented, functions clone, hashCode and equals implemented

--- src/Board/Square.java
+++ src/Board/Square.java
@@ -1,8 +1,9 @@
 package Board;
 
 import Game.Player;
+import java.util.Objects;
 
-public class Square implements Comparable<Square>{
+public class Square implements Comparable<Square>, Cloneable{
     protected int index;
     protected final String name;
 
@@ -24,6 +25,30 @@ public class Square implements Comparable<Square>{
         } else {
             return 1;
         }
+    }
+
+    @Override
+    public Square clone() {
+        try {
+            return (Square) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Cloning not supported for Square");
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Square square = (Square) obj;
+        return index == square.index &&
+                Objects.equals(name, square.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, name);
     }
 }
 
